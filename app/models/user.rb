71d3b66440b7fb9454e50fdad864bcebb123d968
Jason Seminara <jason.seminara@ga.co>
2018-10-30 19:21:03 -0400

    tweeter

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   validates_presence_of :name, :email, :password_digest
   validates :email, uniqueness: true
 
+  has_many :tweets
+
   #encrypt password
   has_secure_password
 end
