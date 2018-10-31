14b1b343a841f717e97dd6bacd4cdd5a65eb5589
Jason Seminara <jason.seminara@ga.co>
2018-10-30 21:29:03 -0400

    cleaned up package.json

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,4 @@
 class Tweet < ApplicationRecord
   belongs_to :user
+  scope :by_user, -> (user) { where user_id: user.id }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :tweets
   post 'auth/register', to: 'users#register'
   post 'auth/login', to: 'users#login'
   get 'test', to: 'users#index'
